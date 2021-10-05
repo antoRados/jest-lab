21b1690ab8726d5188fe20c2636d4d983ac4c72c
antoRados <antorados@gmail.com>
2021-10-05 17:28:38 -0400

    additional challenge questions completed

--- testing-lab/__tests__/functions.test.js
+++ testing-lab/__tests__/functions.test.js
@@ -1,4 +1,5 @@
-const {returnTwo, greeting, add} = require('../functions');
+const {returnTwo, greeting, add, multiply, divide, subtract} = require('../functions');
+
 
 test('returnTwo', () => {
     expect(returnTwo()).toBe(2)
@@ -9,6 +10,21 @@ test('greeting', () => {
     expect(greeting('Jill')).toEqual('Hello, Jill')
 });
 
-test('add', () => {
-    expect(add(1, 2)).toBe(3)
+// Challenge
+describe('Math Functions', () => {
+    test('add', () => {
+        expect(add(1, 2)).toBe(3)
+    });
+    
+    test('multiply', () => {
+        expect(multiply(5, 4)).toBe(20)
+    });
+    
+    test('divide', () => {
+        expect(divide(8, 2)).toBe(4)
+    });
+    
+    test('subtract', () => {
+        expect(subtract(10, 2)).toBe(8)
+    });
 });
